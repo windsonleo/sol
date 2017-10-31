90d17b00455d6b6fbd378064b710fa68c944c2c7
windsonleo <windsor_melo@hotmail.com>
2017-10-31 20:39:13 -0300

    Imports não Usados

--- src/main/java/br/com/tecsoluction/sol/conf/security/ConfiguracaoSecurity.java
+++ src/main/java/br/com/tecsoluction/sol/conf/security/ConfiguracaoSecurity.java
@@ -1,25 +1,14 @@
 package br.com.tecsoluction.sol.conf.security;
 
-import javax.servlet.MultipartConfigElement;
-import javax.servlet.Registration.Dynamic;
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import br.com.tecsoluction.sol.conf.DataSourceConf;
 
--- src/main/java/br/com/tecsoluction/sol/conf/web/WebConfig.java
+++ src/main/java/br/com/tecsoluction/sol/conf/web/WebConfig.java
@@ -1,17 +1,8 @@
 package br.com.tecsoluction.sol.conf.web;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import br.com.tecsoluction.sol.conf.security.ConfiguracaoSecurity;
 
 @Configuration
 //@EnableWebMvc
