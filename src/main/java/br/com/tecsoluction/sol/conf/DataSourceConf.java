777858223625668259b3a7d1357713a9a665908e
windsonleo <windsor_melo@hotmail.com>
2017-10-31 19:33:13 -0300

    Implementação do Bean DataSource

--- src/main/java/br/com/tecsoluction/sol/conf/DataSourceConf.java
+++ src/main/java/br/com/tecsoluction/sol/conf/DataSourceConf.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DataSourceConf {
@@ -15,9 +16,16 @@ public class DataSourceConf {
 	
 	    @Bean
 	    @Primary
-	    @ConfigurationProperties(prefix = "spring.datasource")
+//	    @ConfigurationProperties(prefix = "spring.datasource")
 	    public static DataSource dataSource() {
-	        return DataSourceBuilder.create().build();
+	   
+	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	        dataSource.setDriverClassName("org.postgresql.Driver");
+	        dataSource.setUrl("jdbc:postgresql://sbiymdvltaunnx:a8cfe732a24937cda48d29b651645a84a3efc82966051a0f1b7a29e19a9af16c@ec2-184-73-159-137.compute-1.amazonaws.com:5432/d2imdq1tlu3415");
+	        dataSource.setUsername("sbiymdvltaunnx");
+	        dataSource.setPassword("a8cfe732a24937cda48d29b651645a84a3efc82966051a0f1b7a29e19a9af16c");
+
+	    	return DataSourceBuilder.create().build();
 	    }
 
 
