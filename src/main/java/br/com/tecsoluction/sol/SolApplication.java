514957648b849f599ee7d18fbd74790b7176c0aa
windsonleo <windsor_melo@hotmail.com>
2017-10-31 18:46:09 -0300

    Create na Property

--- src/main/java/br/com/tecsoluction/sol/SolApplication.java
+++ src/main/java/br/com/tecsoluction/sol/SolApplication.java
@@ -175,7 +175,7 @@ public class SolApplication  extends SpringBootServletInitializer {
         Properties ps = new Properties();
 //        ps.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         ps.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        ps.put("spring.jpa.hibernate.ddl-auto", "update");
+        ps.put("spring.jpa.hibernate.ddl-auto", "create");
 		ps.put("useSSL","false");
 //		ps.put("security.basic.enabled","false");
 
