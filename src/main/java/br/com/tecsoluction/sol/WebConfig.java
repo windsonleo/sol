4b6d9fd14396803f88bf1e48b19841060df10f26
windsonleo <windsor_melo@hotmail.com>
2017-11-01 01:10:57 -0300

    Retirado o folder de Source, Deletado Sources do Tomcat, Corrigido classpacth

--- src/main/java/br/com/tecsoluction/sol/WebConfig.java
+++ src/main/java/br/com/tecsoluction/sol/WebConfig.java
@@ -42,18 +42,18 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/src/main/resources/");
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/src/main/resources/static/");
-        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/src/main/resources/templates/");
-        registry.addResourceHandler("/web/**").addResourceLocations("classpath:/src/main/resources/static/web/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/resources/static/");
+        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/resources/templates/");
+        registry.addResourceHandler("/web/**").addResourceLocations("classpath:/resources/static/web/");
         registry.addResourceHandler("**/favicon.ico").addResourceLocations("classpath:/resources/static/img/favicon.ico");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/build/**").addResourceLocations("classpath:/src/main/resources/static/web/build");
-        registry.addResourceHandler("/vendors/**").addResourceLocations("classpath:/src/main/resources/static/web/vendors/");
-        registry.addResourceHandler("/audio/**").addResourceLocations("classpath:/src/main/resources/static/audio/");
-        registry.addResourceHandler("/error/**").addResourceLocations("classpath:/src/main/resources/templates/error/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/src/main/resources/static/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/src/main/resources/static/js/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/build/**").addResourceLocations("classpath:/resources/static/web/build");
+        registry.addResourceHandler("/vendors/**").addResourceLocations("classpath:/resources/static/web/vendors/");
+        registry.addResourceHandler("/audio/**").addResourceLocations("classpath:/resources/static/audio/");
+        registry.addResourceHandler("/error/**").addResourceLocations("classpath:/resources/templates/error/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/resources/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/resources/static/js/");
 
    
     }
