405e0396ddbf40f27235818edaf476983562f32c
windsonleo <windsor_melo@hotmail.com>
2017-10-31 22:04:05 -0300

    webjar e jar bootstrap

--- src/main/java/br/com/tecsoluction/sol/conf/web/WebConfig.java
+++ src/main/java/br/com/tecsoluction/sol/conf/web/WebConfig.java
@@ -32,7 +32,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/src/main/resources/templates/");
         registry.addResourceHandler("/web/**").addResourceLocations("classpath:/src/main/resources/static/web/");
         registry.addResourceHandler("**/favicon.ico").addResourceLocations("classpath:/resources/static/img/favicon.ico");
-//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/build/**").addResourceLocations("classpath:/src/main/resources/static/web/build");
         registry.addResourceHandler("/vendors/**").addResourceLocations("classpath:/src/main/resources/static/web/vendors/");
         registry.addResourceHandler("/audio/**").addResourceLocations("classpath:/src/main/resources/static/audio/");
