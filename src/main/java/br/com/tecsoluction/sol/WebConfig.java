9c8942ba9bedde5a5404c0ab1041a149b170caed
windsonleo <windsor_melo@hotmail.com>
2017-11-01 01:34:18 -0300

    Comentados ResourcesHandles e Incluido th: head complemento de url

--- src/main/java/br/com/tecsoluction/sol/WebConfig.java
+++ src/main/java/br/com/tecsoluction/sol/WebConfig.java
@@ -43,17 +43,17 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/");
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/resources/static/");
-        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/resources/templates/");
-        registry.addResourceHandler("/web/**").addResourceLocations("classpath:/resources/static/web/");
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/resources/static/");
+//        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/resources/templates/");
+//        registry.addResourceHandler("/web/**").addResourceLocations("classpath:/resources/static/web/");
         registry.addResourceHandler("**/favicon.ico").addResourceLocations("classpath:/resources/static/img/favicon.ico");
 //        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/build/**").addResourceLocations("classpath:/resources/static/web/build");
-        registry.addResourceHandler("/vendors/**").addResourceLocations("classpath:/resources/static/web/vendors/");
-        registry.addResourceHandler("/audio/**").addResourceLocations("classpath:/resources/static/audio/");
-        registry.addResourceHandler("/error/**").addResourceLocations("classpath:/resources/templates/error/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/resources/static/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/resources/static/js/");
+//        registry.addResourceHandler("/build/**").addResourceLocations("classpath:/resources/static/web/build");
+//        registry.addResourceHandler("/vendors/**").addResourceLocations("classpath:/resources/static/web/vendors/");
+//        registry.addResourceHandler("/audio/**").addResourceLocations("classpath:/resources/static/audio/");
+//        registry.addResourceHandler("/error/**").addResourceLocations("classpath:/resources/templates/error/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/resources/static/css/");
+//        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/resources/static/js/");
 
    
     }
@@ -70,7 +70,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
  				dispatcherServlet(), "/*");
 
  		registration
- 				.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
+ 				.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME);
 
  		return registration;
 
